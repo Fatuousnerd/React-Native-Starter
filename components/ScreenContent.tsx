5e080aa4cddb4e4c37571874432e1a50326ca086
fatuousnerd <110926333+Fatuousnerd@users.noreply.github.com>
2025-08-09 21:08:01 +0300

    ...

--- components/ScreenContent.tsx
+++ components/ScreenContent.tsx
@@ -5,14 +5,22 @@ export const ScreenContent = () => {
   const OpenRepo = () => {
     Linking.openURL("https://github.com/Fatuousnerd/React-Native-Starter.git")
   }
+  const OpenGit = () => {
+    Linking.openURL("https://github.com/Fatuousnerd/")
+  }
   return (
-    <View className="flex-1 flex-col gap-[25px] items-center justify-center bg-[#0B1215]">
-      <Text className="text-[3rem] font-bold text-[#f0ffff] text-center leading-[4rem]">
-        React Native + Nativewind Starter
+    <View className="flex-1 flex-col gap-[50px] p-[25px] items-center justify-center bg-[#0B1215]">
+      <Text className="text-[4rem] font-bold text-[#f0ffff] text-left leading-[4.5rem]">
+        React Native & NativeWind Starter
       </Text>
-      <TouchableOpacity onPress={OpenRepo}>
-        <Text className="text-[#029bce] text-[2rem] underline">Github Repo</Text>
-      </TouchableOpacity>
+      <View className="w-full flex-row justify-between gap-[25px]">
+        <TouchableOpacity onPress={OpenRepo} className="flex-1">
+          <Text className="text-[#029bce] text-[1.5rem] text-center font-bold p-[10px] bg-[#f0ffff] rounded-lg">Github Repo</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={OpenGit} className="flex-1">
+          <Text className="text-[#ffffff] text-[1.5rem] text-center font-bold p-[10px] bg-[#029bce] rounded-lg">Github Profile</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
